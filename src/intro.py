df6fd75b296cb973ceb5c7bfeead9dc887bf0e34
purplebus <mailtosummer@gmail.com>
2022-07-09 18:42:26 +1000

    added intro exit()

---
+++ src/intro.py
@@ -0,0 +1,36 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+print(bcolors.HEADER +"\n\n\n           * *** ***** ***************************************************************************************** ***** *** *    \n\n\n")
+print("     ||||||||||       ||||||||||     ||||||||||    ||       ||   ||||||||||   ||        ||    |||||||||   ||           ||||||||||")
+print("     ||       ||     ||        ||   ||        ||   ||     ||     ||           ||        ||   ||           ||           ||        ")
+print("     ||       ||     ||        ||   ||        ||   ||    ||      ||           ||        ||   ||           ||           ||        ")
+print("     ||     ||       ||        ||   ||        ||   ||  ||        ||           ||        ||   ||           ||           ||        ")
+print("     ||||||||        ||        ||   ||        ||   ||||           |||||||||   ||||||||||||    |||||||||   ||           |||||||   ")
+print("     ||     ||       ||        ||   ||        ||   ||  ||                ||   ||        ||   ||           ||           ||        ")
+print("     ||       ||     ||        ||   ||        ||   ||   ||               ||   ||        ||   ||           ||           ||        ")
+print("     ||         ||   ||        ||   ||        ||   ||    ||              ||   ||        ||   ||           ||           ||        ")
+print("     ||         ||   ||        ||   ||        ||   ||     ||             ||   ||        ||   ||           ||           ||        ")
+print("     ||        ||    ||        ||   ||        ||   ||      ||            ||   ||        ||   ||           ||      ||   ||        ")
+print("     ||||||||||||     ||||||||||     ||||||||||    ||        ||  |||||||||    ||        ||    |||||||||   ||||||||||   ||        ")
+print("\n\n\n           * *** ***** ***************************************************************************************** ***** *** *    \n\n\n"+ bcolors.ENDC)
+print(bcolors.HEADER  + "HEADER  No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.OKBLUE + "OKBLUE: No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.OKCYAN + "KCYAN: No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.OKBLUE + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.OKGREEN + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.WARNING+ "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.FAIL + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.ENDC + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.BOLD + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+print(bcolors.UNDERLINE + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
+
+
