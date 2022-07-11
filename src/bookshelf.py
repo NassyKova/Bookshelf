0c0848731e599260e4c47f4886d8e87dc30dbefa
purplebus <mailtosummer@gmail.com>
2022-07-11 20:17:54 +1000

    trying to use None response

--- src/bookshelf.py
+++ src/bookshelf.py
@@ -11,39 +11,44 @@ library = _seed()
 #list of options
 def _print_options():
     _intro()
-    print(bcolors.OKBLUE + "1. Open the library" + bcolors.ENDC)
-    print(bcolors.OKBLUE + "2. Open the library with notes" + bcolors.ENDC)
-    print(bcolors.OKBLUE + "3. Add a book to the library" + bcolors.ENDC)
-    print(bcolors.OKBLUE + "4. Delete the book" + bcolors.ENDC)
-    print(bcolors.OKBLUE + "5. Edite the note" + bcolors.ENDC)
-    print(bcolors.OKBLUE + "6. Close the library, exit the bookshelf" + bcolors.ENDC)
-    opt = input(bcolors.OKBLUE + "Select your option (1-6): " + bcolors.ENDC)
+    print(bcolors.OKBLUE + "     1. Open the library" + bcolors.ENDC)
+    print(bcolors.OKBLUE + "     2. Open the library with notes" + bcolors.ENDC)
+    print(bcolors.OKBLUE + "     3. Add a book to the library" + bcolors.ENDC)
+    print(bcolors.OKBLUE + "     4. Delete the book" + bcolors.ENDC)
+    print(bcolors.OKBLUE + "     5. Edite the note" + bcolors.ENDC)
+    print(bcolors.OKBLUE + "     6. Close the library, exit the bookshelf" + bcolors.ENDC)
+    opt = input(bcolors.OKBLUE + "     Choose whhat do you want to do (1-6): " + bcolors.ENDC)
     return opt
 
 
 # adding a book to the library using method add_item
 def _add_book():
     _intro()
-    author = input(bcolors.OKBLUE + "Who is the author?: " + bcolors.ENDC)
-    name = input(bcolors.OKBLUE + f"Which book of {author} you want to add?: " + bcolors.ENDC)
-    note = input(bcolors.OKBLUE + f"What was the {name} about?: " + bcolors.ENDC)
+    author = input(bcolors.OKBLUE + "     Who is the author?: " + bcolors.ENDC)
+    name = input(bcolors.OKBLUE + f"     Which book of {author} you want to add?: " + bcolors.ENDC)
+    note = input(bcolors.OKBLUE + f"     What was {name} about?: " + bcolors.ENDC)
     library._add_item(author, name, note)
-    print(bcolors.OKBLUE + f"\nI've added {name} to your library" + bcolors.ENDC)
+    print(bcolors.OKBLUE + f"\n     I've added {name} to your library" + bcolors.ENDC)
 
 #deleting a book from the library using methid delete_item
 def _delete_book():
+    _intro()
     for item in library.books:
         print(item.name)
 
-    name = input(bcolors.OKBLUE + "Which book you want to delete?: "+ bcolors.ENDC)
+    name = input(bcolors.OKBLUE + "\n\n     Which book you want to delete?: \n\n"+ bcolors.ENDC)
+    # if input is None:
+    if input is NameError:
+        return print(bcolors.OKBLUE + f"     You better type something next time\n" + bcolors.ENDC)
     library._delete_item(name)
 
-# edit a book and the elements using method update_...
+# edit a book and the elements using method edit_item
 def _edit_note():
+    _intro()
     for item in library.books:
         print(item.name)
 
-    name = input(bcolors.OKBLUE + "In which book you want to edit?: "+ bcolors.ENDC)
+    name = input(bcolors.OKBLUE + "\n\n     In which book you want to edit?: \n\n"+ bcolors.ENDC)
     library._edit_item_note(name)
 
 
@@ -56,7 +61,7 @@ while option != "6":
     system('clear')
     if option == "1":
         library._print_library()
-    if option == "2":
+    elif option == "2":
         library._print_library_notes()
     elif option == "3":
         _add_book()
@@ -64,12 +69,15 @@ while option != "6":
         _delete_book()
     elif option == "5":
         _edit_note()
-
+    elif option == "6":
+        continue
     else:
-        print(bcolors.FAIL +"\n\nInvalid option" + bcolors.ENDC)
+        _intro()
+        print(bcolors.FAIL +"     \n\nNo such option, try again\n\n" + bcolors.ENDC)
         
-    input(bcolors.WARNING + "\n\npress Enter to continue..." + bcolors.ENDC)
+    input(bcolors.WARNING + "     \n\npress Enter to continue...\n\n" + bcolors.ENDC)
     system('clear')
 
-print(bcolors.OKBLUE + "\n\nGoodbye!" + bcolors.ENDC) 
+_intro()
+print(bcolors.OKBLUE + "     \n\nGoodbye!\n\n" + bcolors.ENDC) 
 
--- src/library.py
+++ src/library.py
@@ -11,14 +11,14 @@ class Library:
 
     def _print_library(self):
         _intro()
-        print(bcolors.OKBLUE + "Nice to see you again!\nThese books you've read so far: \n" + bcolors.ENDC)
+        print(bcolors.OKBLUE + "     Nice to see you again!\n     These books you've read so far: \n" + bcolors.ENDC)
         for item in self.books:
             item._show_book()
         # print(self._seed())
 
     def _print_library_notes(self):
         _intro()
-        print(bcolors.OKBLUE + "Nice to see you again!\nThese books you've read so far: \n" + bcolors.ENDC)
+        print(bcolors.OKBLUE + "     Here a notes for the book you've read: \n" + bcolors.ENDC)
         for item in self.books:
             item._show_book_notes()
         # print(self._seed())
@@ -34,83 +34,15 @@ class Library:
         for item in self.books:
             if item.name == name:
                 self.books.remove(item)
-                return print(bcolors.OKBLUE + f"I've removed {name} from your library \n" + bcolors.ENDC)
+                return print(bcolors.OKBLUE + f"     I've removed {name} from your library \n" + bcolors.ENDC)
                 
-        return print(bcolors.OKBLUE + f"Hm, I couldn't find {name} in your library...\n" + bcolors.ENDC)
+        return print(bcolors.OKBLUE + f"     Hm, I couldn't find {name} in your library...\n" + bcolors.ENDC)
 
     def _edit_item_note(self, name):
         for item in self.books:
             if item.name == name:
-                note = input (bcolors.OKBLUE + f"What is the new description of {name}?: " + bcolors.ENDC)
+                note = input (bcolors.OKBLUE + f"     What is the new description of {name}?: " + bcolors.ENDC)
                 item.note = note
-                return print(bcolors.OKBLUE + f"I've updated the description of {name} \n" + bcolors.ENDC)
-        return print(bcolors.OKBLUE + f"Hm, I couldn't find {name} in your library...\n" + bcolors.ENDC)
+                return print(bcolors.OKBLUE + f"     I've updated the description of {name} \n" + bcolors.ENDC)
+        return print(bcolors.OKBLUE + f"     Hm, I couldn't find {name} in your library...\n" + bcolors.ENDC)
 
-
-
-
-# book01 = Book("New_Author", "New_Book", "Note")
-# book02 = Book("New_Author1", "New_Book1", "Not1e")
-# book03 = Book("New_Author2", "New_Book2", "Note2")
-
-# library = Library([book01, book02, book03])
-# library._print_library()
-
-
-
-
-
-
-
-#     def _add_note(self, name, note):
-#         for item in self.books:
-#             if item.name == name:
-#                 note = (f"What would you like to add as a note for {name}?: ")
-#                 if () == None:
-#                     print("No notes? Fine by me")
-#                 return print(f"A note for {name} was added")
-#         return print(f"I could not find {name} in your library")
-
-
-
-
-#_-----------------------------
-# class Menu:
-#     # a list of menu items
-#     def __init__(self, menu_items):
-#         self.menu_items = menu_items = !!!books
-
-#     def print_menu(self):
-#         print("Welcome to Coder Cafe, this is our menu:")
-#         for item in self.menu_items:
-#             item.show_item() = !!!open_library
-#     # adds a new item to the menu
-    
-    # def add_item(self, name, price):
-    #     #self.menu_items.append(MenuItem(name, price))
-    #     new_item = MenuItem(name, price) = !!!Book
-    #     self.menu_items.append(new_item)
-
-    # def delete_item(self, name):
-    #     # iterate through the list looking for the item
-    #     for item in self.menu_items:
-    #         # check if the item's name of this iteration is equal to the name we receive.
-    #         if item.name == name:
-    #             # access to the list and remove the item
-    #             self.menu_items.remove(item)
-    #             return print(f"{name} was removed from the menu")
-    
-    #     return  print(f"{name} is not in the menu")   
-
-    # def update_price(self, name):
-    #     # iterate through the list looking for the item
-    #     for item in self.menu_items:
-    #         # check if the item's name of this iteration is equal to the name we receive.
-    #         if item.name == name:
-    #             # ask for the new price
-    #             price = float(input(f"What is the new price for {name}? "))
-    #             #update the item's price
-    #             item.price = price
-    #             return print(f"{name}'s price was uptaded in the menu")
-    
-    #     return  print(f"{name} is not in the menu")   
