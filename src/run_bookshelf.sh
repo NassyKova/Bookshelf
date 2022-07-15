84948c6d29055e2da0f6bff9752bd7d80941a476
purplebus <mailtosummer@gmail.com>
2022-07-15 17:01:07 +1000

    finishing with sh file

--- src/run_bookshelf.sh
+++ src/run_bookshelf.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo -n "Print help to read help file or hit enter to open the app: "
-read VAR
-    if [$1 == help]
-    then
-        content = cat help_file.txt
-    else
-        python3 bookshelf.py
+if [ $# -eq 1 ] && [ $1 == "help" ]
+then
+    cat help.txt
+else
+    python3 bookshelf.py
+fi
