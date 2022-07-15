449b216c69be15e76bb249cd1be6e71baa6f8aef
purplebus <mailtosummer@gmail.com>
2022-07-15 16:24:34 +1000

    a bit of progress with sh file

--- src/run_bookshelf.sh
+++ src/run_bookshelf.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-if ! [[ -x "$(command -v python3)" ]]
-then
-    echo 'Error: 
-    This program runs on Python, but it looks like Python is not installed.
-    To install Python, check out https://installpython3.com/' >&2
-    exit 1
-fi
-    python3 bookshelf.py
-
+echo -n "Print help to read help file or hit enter to open the app: "
+read VAR
+    if [$1 == help]
+    then
+        content = cat help_file.txt
+    else
+        python3 bookshelf.py
