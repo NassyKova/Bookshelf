a3971a91504c3478ce6ff11ccda576d90877822f
purplebus <mailtosummer@gmail.com>
2022-07-15 15:50:01 +1000

    adding to sh file

--- src/run_bookshelf.sh
+++ src/run_bookshelf.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-check_python.sh script
-python3 bookshelf.py
+if ! [[ -x "$(command -v python3)" ]]
+then
+    echo 'Error: 
+    This program runs on Python, but it looks like Python is not installed.
+    To install Python, check out https://installpython3.com/' >&2
+    exit 1
+fi
+    python3 bookshelf.py
+
